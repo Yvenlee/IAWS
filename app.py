134143e580a40ebb4c7e4db6e939c1869d761bfc
vyvenlee <yvenlee.voninkabel@ynov.com>
2025-01-09 11:33:58 +0100

    matrix

---
+++ app.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+matrix = np.array([
+    [12,45,78,89],
+    [56,78,90,123],
+    [34, 56, 78, 89],
+    [45,67,78,89],
+    [23,45,67,78],
+    [78,90,123,145],
+    [34,56,78,89]
+])
+
+X_min = np.min(matrix)
+X_max = np.max(matrix)
+normalized_matrix = (matrix - X_min) / (X_max - X_min)
+
+print(normalized_matrix)
